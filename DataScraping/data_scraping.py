181b56d1b07db52d3097e4c9e355da462aea5f80
James <Jamestngo02@gmail.com>
2025-01-26 15:16:23 -0500

    seperate utils and data scrapping

--- DataScraping/data_scraping.py
+++ DataScraping/data_scraping.py
@@ -1,54 +1,25 @@
 import requests
 from bs4 import BeautifulSoup as soup
 import csv 
+from utils import scrape_url
 
-"""
-Beautiful soup is used for parsing html or xml documents . It helps extract specific data web pages like
-titles , linkes or any other HTML elements
-
-"""
-
-
+# URLs for the stats for each stat category
 passing_qb_page = requests.get('https://www.nfl.com/stats/player-stats/')
 rushing_page = requests.get('https://www.nfl.com/stats/player-stats/category/rushing/2024/post/all/rushingyards/desc')
 receiving_page = requests.get('https://www.nfl.com/stats/player-stats/category/receiving/2024/post/all/receivingreceptions/desc')
 
+# headers from NFL stats sites for each stat category
+passing_yards_headers = ["Player Name", "Passing Yards", " Yards/Attempt", "Attempt","Completions","Completions Percentage","Touchdowns","Interceptions","Rating","1st","1st%","20+", "40+", "Lng" , "sack", "sack yards"]
+rushing_yards_headers = ["Player Name", "Rushing Yards", "attempts", "Touchdowns", "20 plus yards", "40 plus yards", "Long", "rushing 1st down", "Rushing 1st % " , "Fumbles"]
+receiving_yards_headers = ["Player Name","Receptions", "Receiving Yards","Touchdowns", "20 plus yards", "40 plus yards", "Long", "1st down receptions", "1st % " , "fumbles " ," yac yards ber recpetions " , "targets"]
 
 
+passing_csv = "passingYards.csv"
+receiving_csv = "receivingYards.csv"
+rushing_csv = "rushingYards.csv"
 
 
-""" convert to beautiful soup object """
-webpage = soup(passing_qb_page.content, features='html.parser')
-
-passing_qb_data = []
-
-# web scrape the web page URL , load in given array
-for row in webpage.tbody.find_all('tr'):
-    cols = row.find_all('td')
-    
-    # Player name | Pass Yards | yds / att | att | cmp | cmp % | td | int | rate | 1st | 1st% | 20 yd plays | 40 yard plays | long pass play | sacks | sack yards
-
-    # Loop through each <td> element to get the text (or you can process it as needed)
-    data = []
-    for col in cols:
-       
-        data.append(col.get_text().strip())
-
-    passing_qb_data.append(data)
-
-
-qb_passing_file = 'qbPassing.csv'
-print(passing_qb_data)
-
-# write to the file 
-with open(qb_passing_file, mode="w", encoding="utf-8" , newline='') as file:
-    file.write = csv.writer(file)
-
-    file.write.writerow(["Player Name", "Passing Yards", " Yards/Attempt", "Attempt","Completions","Completions Percentage","Touchdowns","Interceptions","Rating","1st","1st%","20+", "40+", "Lng" , "sack", "sack yards"])
-
-    for array in passing_qb_data:
-        file.write.writerow(array)
-
+scrape_url(rushing_page,rushing_yards_headers,rushing_csv)
 
 
 
--- DataScraping/utils.py
+++ DataScraping/utils.py
@@ -1,3 +1,11 @@
+import requests
+from bs4 import BeautifulSoup as soup
+import csv 
+"""
+Beautiful soup is used for parsing html or xml documents . It helps extract specific data web pages like
+titles , linkes or any other HTML elements
+
+"""
 
 def scrape_url(url, header_rows , csv_file):
 
@@ -18,13 +26,15 @@ def scrape_url(url, header_rows , csv_file):
             data.append(col.get_text().strip())
 
         data_array.append(data)
+    write_to_csv(header_rows,csv_file,data_array)
+
 
 
 def write_to_csv(header_rows , csv_file , array_data):
     with open(csv_file, "w", encoding="utf-8") as file:
-    file.write = csv.writer(file)
+        file.write = csv.writer(file)
 
-    file.write.writerows(header_rows)
+        file.write.writerows(header_rows)
 
-    for row in array_data:
-        file.write.writerows(row)
+        for row in array_data:
+            file.write.writerows(row)
